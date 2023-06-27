4135aa715f4e0155bb1306753e1328734622d2e0
Andy Zhou <zhoua2001@gmail.com>
2023-06-27 16:25:40 -0400

    changed to side navigation drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mouse_irl_website/screens/home.dart';
 import 'package:mouse_irl_website/screens/calendar.dart';
+import 'package:mouse_irl_website/screens/user.dart';
 // import 'package:provider/provider.dart';
 
 void main() {
@@ -19,19 +20,81 @@ class _MyAppState extends State<MyApp> {
   static const List<Widget> _pages = <Widget>[
     HomePage(),
     CalendarPage(),
-    Placeholder(),
+    UserPage(),
   ];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        colorScheme: const ColorScheme(
+          brightness: Brightness.light,
+
+          primary: Colors.purpleAccent,
+          onPrimary: Colors.black,
+
+          secondary: Colors.purpleAccent,
+          onSecondary: Colors.black,
+
+          background: Colors.white,
+          onBackground: Colors.black,
+
+          surface: Colors.white,
+          onSurface: Colors.black,
+
+          error: Colors.red,
+          onError: Colors.black,
+        ),
+      ),
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.purpleAccent,
           title: const Text('Title!!!!!:3'),
         ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+                child: Text('Drawer Header'),
+              ),
+              ListTile(
+                leading: Icon(Icons.home),
+                title: const Text('Home'),
+                onTap: () {
+                  setState(() {
+                    _selectedIndex = 0;
+                  });
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.calendar_today),
+                title: const Text('Calendar'),
+                onTap: () {
+                  setState(() {
+                    _selectedIndex = 1;
+                  });
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.person),
+                title: const Text('User'),
+                onTap: () {
+                  setState(() {
+                    _selectedIndex = 2;
+                  });
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          )
+        ),
         body: _pages.elementAt(_selectedIndex),
-        bottomNavigationBar: bottomNavBar(),
+        // bottomNavigationBar: bottomNavBar(),
       )
     );
   }
