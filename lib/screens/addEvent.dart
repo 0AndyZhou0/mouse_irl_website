90c3ca17b81791c28f225a8100057059e6816f3d
Andy Zhou <zhoua2001@gmail.com>
2023-08-02 00:28:58 -0400

    remove unecessary

--- lib/screens/addEvent.dart
+++ lib/screens/addEvent.dart
@@ -1,6 +1,4 @@
-import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
-import 'package:mouse_irl_website/auth.dart';
 import 'package:mouse_irl_website/database.dart';
 
 class AddEvent extends StatefulWidget {
