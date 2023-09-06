71df3b532c47e6e00ee64d9c299bc49df7d635e3
Andy Zhou <zhoua2001@gmail.com>
2023-09-05 22:12:11 -0400

    removed unecessary stuffs

--- lib/screens/addTime.dart
+++ lib/screens/addTime.dart
@@ -12,8 +12,8 @@ class AddTime extends StatefulWidget {
 
 class _AddTimeState extends State<AddTime> {
   DateTime _dateTime = DateTime.now();
-  DateTime _date = DateTime.now();
-  DateTime _time = DateTime.now();
+  // DateTime _date = DateTime.now();
+  // DateTime _time = DateTime.now();
 
   void onDateTimeChanged(DateTime newDateTime) {
     setState(() {
@@ -21,63 +21,63 @@ class _AddTimeState extends State<AddTime> {
     });
   }
 
-  void onDateChanged(DateTime newDate) {
-    setState(() {
-      _date = newDate;
-    });
-  }
+  // void onDateChanged(DateTime newDate) {
+  //   setState(() {
+  //     _date = newDate;
+  //   });
+  // }
 
-  void onTimeChanged(DateTime newTime) {
-    setState(() {
-      _time = newTime;
-    });
-  }
+  // void onTimeChanged(DateTime newTime) {
+  //   setState(() {
+  //     _time = newTime;
+  //   });
+  // }
 
   //taken from https://pub.dev/packages/datetime_picker_formfield_new
-  Widget basicDateField() {
-    final format = DateFormat("yyyy-MM-dd");
-    return Column(children: <Widget>[
-      Text('Basic date field (${format.pattern})'),
-      DateTimeField(
-        format: format,
-        onShowPicker: (context, currentValue) {
-          return showDatePicker(
-            context: context,
-            firstDate: DateTime(1900),
-            initialDate: currentValue ?? DateTime.now(),
-            lastDate: DateTime(2100),
-          );
-        },
-        onChanged: (value) {
-          if (value != null) {
-            onDateChanged(value);
-          }
-        },
-      ),
-    ]);
-  }
+  // Widget basicDateField() {
+  //   final format = DateFormat("yyyy-MM-dd");
+  //   return Column(children: <Widget>[
+  //     Text('Basic date field (${format.pattern})'),
+  //     DateTimeField(
+  //       format: format,
+  //       onShowPicker: (context, currentValue) {
+  //         return showDatePicker(
+  //           context: context,
+  //           firstDate: DateTime(1900),
+  //           initialDate: currentValue ?? DateTime.now(),
+  //           lastDate: DateTime(2100),
+  //         );
+  //       },
+  //       onChanged: (value) {
+  //         if (value != null) {
+  //           onDateChanged(value);
+  //         }
+  //       },
+  //     ),
+  //   ]);
+  // }
 
-  Widget basicTimeField() {
-    final format = DateFormat("HH:mm");
-    return Column(children: <Widget>[
-      Text('Basic time field (${format.pattern})'),
-      DateTimeField(
-        format: format,
-        onShowPicker: (context, currentValue) async {
-          final time = await showTimePicker(
-            context: context,
-            initialTime: TimeOfDay.fromDateTime(currentValue ?? DateTime.now()),
-          );
-          return DateTimeField.convert(time);
-        },
-        onChanged: (value) {
-          if (value != null) {
-            onTimeChanged(value);
-          }
-        },
-      ),
-    ]);
-  }
+  // Widget basicTimeField() {
+  //   final format = DateFormat("HH:mm");
+  //   return Column(children: <Widget>[
+  //     Text('Basic time field (${format.pattern})'),
+  //     DateTimeField(
+  //       format: format,
+  //       onShowPicker: (context, currentValue) async {
+  //         final time = await showTimePicker(
+  //           context: context,
+  //           initialTime: TimeOfDay.fromDateTime(currentValue ?? DateTime.now()),
+  //         );
+  //         return DateTimeField.convert(time);
+  //       },
+  //       onChanged: (value) {
+  //         if (value != null) {
+  //           onTimeChanged(value);
+  //         }
+  //       },
+  //     ),
+  //   ]);
+  // }
 
   Widget basicDateTimeField() {
     final format = DateFormat("yyyy-MM-dd HH:mm");
