fa05c12c4bc1f1fd891c766ab76369113b90c0f8
Andy Zhou <zhoua2001@gmail.com>
2023-11-03 23:34:51 -0400

    added base for email verification

--- lib/auth.dart
+++ lib/auth.dart
@@ -5,11 +5,22 @@ class Auth {
 
   User? get currentUser => _auth.currentUser;
 
+  String _token = '';
+
   Stream<User?> get authStateChanges => _auth.authStateChanges();
 
-  Future<void> signInWithEmailAndPassword(
-      {required String email, required String password}) async {
-    await _auth.signInWithEmailAndPassword(email: email, password: password);
+  Future<void> _authenticate({
+    required String email,
+    required String password,
+    required String urlSegment,
+  }) async {
+    var acs = ActionCodeSettings(url: 'https://mouseirl.firebaseapp.com');
+    FirebaseAuth.instance
+        .sendSignInLinkToEmail(email: email, actionCodeSettings: acs)
+        .catchError(
+            (onError) => print('Error sending email verification $onError'))
+        .then((value) => print('Successfully sent email verification'));
+    //TODO: change to logger
   }
 
   Future<void> createUserWithEmailAndPassword(
@@ -18,7 +29,23 @@ class Auth {
         email: email, password: password);
   }
 
+  Future<void> signInWithEmailAndPassword(
+      {required String email, required String password}) async {
+    await _auth.signInWithEmailAndPassword(email: email, password: password);
+  }
+
   Future<void> signOut() async {
     await _auth.signOut();
+  }
+
+  Future<bool> isLoggedIn() async {
+    return currentUser != null;
+  }
+
+  Future<bool> isEmailVerified() async {
+    if (currentUser == null) {
+      return false;
+    }
+    return currentUser!.emailVerified;
   }
 }
