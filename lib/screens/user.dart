edcae316c50e8c6caf5e6c7055211a569551c1a9
Andy Zhou <zhoua2001@gmail.com>
2024-01-18 10:34:37 -0500

    added more user info

--- lib/screens/user.dart
+++ lib/screens/user.dart
@@ -5,6 +5,10 @@ import 'package:mouse_irl_website/database.dart';
 import 'package:mouse_irl_website/screens/admin_event.dart';
 import 'package:mouse_irl_website/screens/admin_time.dart';
 
+// TODO: merge user page with home page
+// Profile goes in the corner
+// Edit buttons directly on voting boxes
+
 class UserPage extends StatefulWidget {
   const UserPage({super.key});
 
@@ -17,6 +21,18 @@ class _UserPageState extends State<UserPage> {
 
   Widget _userEmail() {
     return Text(user?.email ?? '');
+  }
+
+  Widget _userProfilePicture() {
+    // TODO: turn into button that goes to user profile edit page
+    return CircleAvatar(
+      radius: 20,
+      backgroundImage: NetworkImage(user?.photoURL ?? ''),
+    );
+  }
+
+  Widget _userProfile() {
+    return Text(user?.displayName ?? '');
   }
 
   Widget _userSignOutButton() {
@@ -75,30 +91,49 @@ class _UserPageState extends State<UserPage> {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            _userEmail(),
-            const SizedBox(height: 20),
-            FutureBuilder(
-              future: Database().isAdmin(user!.uid),
-              builder: (context, snapshot) {
-                if (snapshot.hasData && snapshot.data == true) {
-                  return Column(children: [
-                    _adminEventsPageButton(),
-                    const SizedBox(height: 20),
-                    _adminTimesPageButton(),
-                  ]);
-                } else {
-                  return const SizedBox.shrink();
-                }
-              },
-              initialData: const Text('Loading :3'),
+        child: Container(
+          alignment: Alignment.center,
+          child: SizedBox(
+            width: 600,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    _userProfilePicture(),
+                    const SizedBox(width: 20),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        _userProfile(),
+                        _userEmail(),
+                      ],
+                    )
+                  ],
+                ),
+                const SizedBox(height: 20),
+                FutureBuilder(
+                  future: Database().isAdmin(user!.uid),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData && snapshot.data == true) {
+                      return Column(children: [
+                        _adminEventsPageButton(),
+                        const SizedBox(height: 20),
+                        _adminTimesPageButton(),
+                      ]);
+                    } else {
+                      return const SizedBox.shrink();
+                    }
+                  },
+                  initialData: const Text('Loading :3'),
+                ),
+                const SizedBox(height: 20),
+                _userSignOutButton(),
+              ],
             ),
-            const SizedBox(height: 20),
-            _userSignOutButton(),
-          ],
+          ),
         ),
       ),
     );
