54594e5ed40d1bb7ce130b1c18b8f09d61cf5cd9
Andy Zhou <zhoua2001@gmail.com>
2024-04-12 19:00:01 -0400

    added a lot of logging

--- lib/auth.dart
+++ lib/auth.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
+import 'package:logging/logging.dart';
 
 class Auth {
   final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -9,6 +10,8 @@ class Auth {
   User? get currentUser => _auth.currentUser;
   User? googleUser;
   String? name, imageUrl, userEmail, uid;
+
+  final Logger _logger = Logger('Auth');
 
   // final GoogleSignInPlugin googleSignIn = GoogleSignInPlugin();
 
@@ -32,45 +35,86 @@ class Auth {
 
   Future<void> createUserWithEmailAndPassword(
       {required String email, required String password}) async {
-    await _auth.createUserWithEmailAndPassword(
-        email: email, password: password);
+    _logger.fine('Attempting to create a user with email and password');
+
+    if (isLoggedIn()) {
+      _logger.fine('Already logged in');
+      return;
+    }
+
+    try {
+      await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      _logger.fine('User creation with email and password succeeded');
+    } on FirebaseAuthException catch (e) {
+      _logger.severe('Failed to create a user with email and password', e);
+    }
   }
 
   Future<void> signInWithEmailAndPassword(
       {required String email, required String password}) async {
-    await _auth.signInWithEmailAndPassword(email: email, password: password);
+    _logger.fine('Attempting to sign in with email and password');
+
+    if (isLoggedIn()) {
+      _logger.fine('Already logged in');
+      return;
+    }
+
+    try {
+      await _auth.signInWithEmailAndPassword(email: email, password: password);
+      _logger.fine('Sign in with email and password succeeded');
+    } on FirebaseAuthException catch (e) {
+      _logger.warning('Failed to sign in with email and password', e);
+    }
   }
 
   Future<void> signInWithGoogle() async {
+    _logger.fine('Signing in with Google');
+
+    if (isLoggedIn()) {
+      _logger.fine('Already logged in');
+      return;
+    }
+
     GoogleAuthProvider googleProvider = GoogleAuthProvider();
 
     try {
       //web
       if (kIsWeb) {
+        _logger.finer('Signing in with Google on web');
         await _auth.signInWithRedirect(googleProvider);
       } else if (Platform.isAndroid || Platform.isIOS) {
+        _logger.finer('Signing in with Google on mobile');
         // TODO: implement
-        // await _auth.signInWithProvider(googleProvider);
+        await _auth.signInWithProvider(googleProvider);
       }
     } catch (e) {
-      // print(e);
+      _logger.warning('Error signing in with Google: $e');
     }
 
+    // _logger.finest('Google user: $googleUser');
+
     if (googleUser != null) {
+      _logger.fine('Signing in with Google succeeded');
       name = googleUser!.displayName;
       imageUrl = googleUser!.photoURL;
       userEmail = googleUser!.email;
       uid = googleUser!.uid;
 
-      // print("name: $name");
-      // print("userEmail: $userEmail");
-      // print("imageUrl: $imageUrl");
+      _logger.finest('name: $name');
+      _logger.finest('userEmail: $userEmail');
+      _logger.finest('imageUrl: $imageUrl');
     }
   }
 
   Future<void> signOut() async {
+    _logger.fine('Attempting to sign out');
+
     if (isLoggedIn()) {
       await _auth.signOut();
+      _logger.fine('Sign out successful');
+    } else {
+      _logger.fine('User is not logged in, sign out aborted');
     }
   }
 
@@ -79,7 +123,7 @@ class Auth {
   }
 
   Future<bool> isEmailVerified() async {
-    if (currentUser == null) {
+    if (!isLoggedIn()) {
       return false;
     }
     return currentUser!.emailVerified;
@@ -92,34 +136,63 @@ class Auth {
   // }
 
   Future<void> sendPasswordResetEmail({required String email}) async {
-    await _auth.sendPasswordResetEmail(email: email);
+    _logger.fine('Attempting to send password reset email to $email');
+    try {
+      await _auth.sendPasswordResetEmail(email: email);
+      _logger.fine('Password reset email sent successfully to $email');
+    } catch (e) {
+      _logger.severe('Failed to send password reset email to $email', e);
+    }
   }
 
   Future<void> getUserProfile() async {
-    if (currentUser == null) {
+    if (!isLoggedIn()) {
+      _logger.finest('User not logged in, aborting...');
       return;
     }
     name = currentUser!.displayName;
+    _logger.finest('Name retrieved: $name');
     imageUrl = currentUser!.photoURL;
+    _logger.finest('Image URL retrieved: $imageUrl');
     userEmail = currentUser!.email;
+    _logger.finest('Email retrieved: $userEmail');
     uid = currentUser!.uid;
+    _logger.finest('UID retrieved: $uid');
   }
 
   Future<void> updateUserName(String? name) async {
-    if (currentUser == null) {
+    _logger.finest('Attempting to update user name');
+    if (!isLoggedIn()) {
+      _logger.finest('User not logged in, aborting...');
       return;
     }
+    _logger.fine('Updating user name to $name');
     if (name != null && name != '') {
-      currentUser!.updateDisplayName(name);
+      try {
+        await currentUser!.updateDisplayName(name);
+        _logger.fine('User name updated to $name');
+      } catch (e) {
+        _logger.warning('Failed to update user name to $name', e);
+      }
     }
   }
 
   Future<void> updateUserProfilePic(String? imageUrl) async {
-    if (currentUser == null) {
+    _logger.finest('Attempting to update user profile picture');
+    if (!isLoggedIn()) {
+      _logger.finest('User not logged in, aborting...');
       return;
     }
+    _logger.fine('Updating user profile picture to $imageUrl');
     if (imageUrl != null && imageUrl != '') {
-      currentUser!.updatePhotoURL(imageUrl);
+      try {
+        await currentUser!.updatePhotoURL(imageUrl);
+        _logger.fine('User profile picture updated to $imageUrl');
+      } catch (e) {
+        _logger.severe('Failed to update user profile picture to $imageUrl', e);
+      }
+    } else {
+      _logger.finer('No image URL provided, skipping profile picture update');
     }
   }
 }
